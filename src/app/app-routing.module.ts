c68381fdb6eaae8d3e06efb5614a094c38a94151
William Noris <w.noris@protonmail.com>
2021-12-15 16:45:21 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAccueilComponent } from './pages/page-accueil/page-accueil.component';
+import { PageDetailsComponent } from './pages/page-details/page-details.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: PageAccueilComponent },
+  { path: 'details', component: PageDetailsComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
