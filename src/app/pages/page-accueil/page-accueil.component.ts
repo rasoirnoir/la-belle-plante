d17ea932f87d098a08a0d966ed09c667d600a5bc
William Noris <w.noris@protonmail.com>
2021-12-16 11:16:14 +0100

    Mise en place des catégories du filtre

--- src/app/pages/page-accueil/page-accueil.component.ts
+++ src/app/pages/page-accueil/page-accueil.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'underscore';
+import { list_products } from 'src/app/data';
 
 @Component({
-  selector: 'app-page-accueil',
-  templateUrl: './page-accueil.component.html',
-  styleUrls: ['./page-accueil.component.scss']
+    selector: 'app-page-accueil',
+    templateUrl: './page-accueil.component.html',
+    styleUrls: ['./page-accueil.component.scss'],
 })
 export class PageAccueilComponent implements OnInit {
+    public filterCat!: string[];
 
-  constructor() { }
+    constructor() {
+        this.filterCat = [];
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.filterCat = this.extractCategories();
+    }
 
+    extractCategories(): string[] {
+        const categories = list_products.map(
+            (value) => value['product_breadcrumb_label']
+        );
+        return _.uniq(categories);
+    }
 }
